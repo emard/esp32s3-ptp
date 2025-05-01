10c53af0d35a2d14bc3e7bd923a447209a953a3e
Emard <vordah@gmail.com>
2025-05-01 12:44:44 +0200

    tree.py: generate data structure similar to something PTP will ask for

--- debug/tree.py
+++ debug/tree.py
@@ -11,6 +11,7 @@ next_handle=0
 
 path2handle={}
 handle2path={}
+dir2handles={}
 
 DIR=const(16384)
 #FILE=const(32768)
@@ -19,27 +20,46 @@ DIR=const(16384)
 # recurse: number of subdirectorys to recurse into
 def ls(path,recurse):
   global next_handle
-  path2handle[path]=next_handle
-  handle2path[next_handle]=path
-  next_handle+=1
-
-  dir=os.ilistdir(path)
+  try:
+    dir=os.ilistdir(path)
+  except:
+    return
+  # path -> handle and handle -> path
+  if path in path2handle:
+    current_dir=path2handle[path]
+  else:
+    current_dir=next_handle
+    next_handle+=1
+    path2handle[path]=current_dir
+    handle2path[current_dir]=path
+    if path=="/":
+      print("root", current_dir)
+  # id -> directory list
+  if not current_dir in dir2handles:
+    dir2handles[current_dir]={}
   for obj in dir:
     if path=="/":
       fullpath="/"+obj[0]
     else:
       fullpath=path+"/"+obj[0]
+    if fullpath in path2handle:
+      current_handle=path2handle[fullpath]
+    else:
+      current_handle=next_handle
+      next_handle+=1
+    dir2handles[current_dir][current_handle]=obj[0]
     if obj[1]==DIR:
       print(path,"DIR:",obj)
       if recurse>0:
         ls(fullpath,recurse-1)
-    else: # FILE
+    else: # obj[1]==FILE
       print(path,"FILE:",obj)
-      path2handle[fullpath]=next_handle
-      handle2path[next_handle]=fullpath
-      next_handle+=1
+      if not fullpath in path2handle:
+        path2handle[fullpath]=current_handle
+        handle2path[current_handle]=fullpath
 
+ls("/",3)
 ls("/",3)
 print(handle2path)
 print(path2handle)
-    
+print(dir2handles)
